2e1dfcb74872f041c6be5d14e1ec3465c9206deb
PERRYNEMO <perrynemoo@gmail.com>
2022-05-28 15:29:21 +0200

    add TabCompletor

--- src/main/java/net/perrynemo/psapi/perryspigotapi/Command/Command.java
+++ src/main/java/net/perrynemo/psapi/perryspigotapi/Command/Command.java
@@ -2,13 +2,16 @@ package net.perrynemo.psapi.perryspigotapi.Command;
 
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 
-public abstract class Command implements CommandExecutor {
+public abstract class Command implements CommandExecutor, TabCompleter {
 
     private final CommandInfo commandInfo;
     protected final JavaPlugin main;
@@ -43,4 +46,25 @@ public abstract class Command implements CommandExecutor {
     }
     public void execute(CommandSender sender, String[] args){}
     public void execute(Player player, String[] args){}
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
+        List<String> completors = new ArrayList<>();
+        if (!commandInfo.permissions().isEmpty()) {
+            if (!sender.hasPermission(commandInfo.permissions())) {
+                sender.sendMessage("You don't have permission to execute this command");
+                return completors;
+            }
+        }
+        if (!commandInfo.requirePlayer()) {
+            return tabComplete(sender, args);
+        }
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("executor must be a player");
+            return  completors;
+        }
+        return tabComplete((Player) sender, args);
+    }
+    public List<String> tabComplete(CommandSender sender, String[] args){return null;}
+    public List<String> tabComplete(Player pLayer, String[] args){return null;}
 }
